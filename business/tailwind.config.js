74e1e74336de3b882ed760dc9329b24e643912d3
Philipp <139921614+Philippcmd@users.noreply.github.com>
2025-03-14 19:36:48 +0100

    Create tailwind.config.js

---
+++ business/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './business/**/*.html',   // Path to your HTML files
+    './business/**/*.js',     // Path to your JS files
+    './business/input.tailwindcss.css' // Input CSS file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
